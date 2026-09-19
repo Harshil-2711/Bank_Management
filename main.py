0134eb5f91168de35c4e25af1b87117bbe0f78f7
Harshil-2711 <harshiljain2711@gmail.com>
2026-09-19 21:27:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("press 1 for creating an account")
+print("press 2 for deposting money")
+print("press 3 for withdrawing money")
+print("press 4 for details")
+print("press 5 for updating the details")
+print("press 6 for deleting account")
+
+a=int(input("tell your response: "))
+if(a==1):
+    
